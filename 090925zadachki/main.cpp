b2a424f61c4b10a2f3692bdd54db87bf785f7af8
nizer29 <164919601+nizer29@users.noreply.github.com>
2025-09-09 12:12:17 +0300

    test1

---
+++ 090925zadachki/main.cpp
@@ -0,0 +1,73 @@
+////zadacha 1
+// #include <string>
+// #include <iostream>
+// int main() {
+    
+//     int a, b;
+//     std::cout << "Vvedite chislo a:\n";
+//     std::cin >> a;
+//     std::cout << "Vvedite chislo b:\n";
+//     std::cin >> b;
+//     std::cout << "Summa:\n" << a+b;
+// }
+
+////zadacha 2
+// #include <string>
+// #include <iostream>
+// int main() {
+    
+//     int a, b;
+//     std::cout << "Vvedite chislo a & b:\n";
+//     std::cin >> a >> b;
+//     std::cout << "Summa, raznost, proizvedenie, chastnoe:\n";
+//     std::cout << a+b << ',' << a-b << ',' << a*b << ',' << a/b;
+// }
+
+////zadacha 3
+// #include <string>
+// #include <iostream>
+// #include <cmath>
+// int main() {
+    
+//     int a, b;
+//     std::cout << "Vvedite kateti:\n";
+//     std::cin >> a >> b;
+//     std::cout << "Gipotenuza ravna:\n";
+//     std::cout << sqrt(a*a + b*b);
+// }
+
+
+// //zadacha 4
+// #include <string>
+// #include <iostream>
+// int main() {
+    
+//     int s, s1, v, t;
+//     s = 109;
+//     std::cout << "Vvedite skorost i vremya v puti:\n";
+//     std::cin >> v >> t;
+//     s1 = v*t;
+//     if (s1>=109);
+//     s1 = s1%109;
+//     std::cout << "Vasya na otmetke:\n";
+//     std::cout << s1;
+// }
+
+// //zadacha 5
+// #include <string>
+// #include <iostream>
+// int main() {
+    
+//     int h, mm, ss,ooo;
+//     std::cout << "Vvedite secundi:\n";
+//     std::cin >> ooo;
+//     h = ooo/3600;
+//     mm = ooo%3600/60;
+//     ss = ooo%60;
+//     std::cout << "Vremya na chasax:";
+//     if (mm<10) ;
+//     std::cout << h << ":" << mm << ":" << ss;
+// }
+
+
+
