a2962282e42c62e0d7490d56c869f2fead5fa2c0
nizer29 <164919601+nizer29@users.noreply.github.com>
2025-09-16 11:57:41 +0300

    aoaao

---
+++ practice2/main.cpp
@@ -0,0 +1,63 @@
+// // zadacha 3.1
+// #include <iostream>
+// #include <cmath>
+// #include <iomanip>
+// using namespace std;
+// int main() {
+//     double x ,y;
+//     int d=1;
+//     cout << "skolko sper v 1 den: \n";
+//     cin >> x;
+//     cout << "skolko nuzhno chtob nakukanili: \n";
+//     cin >> y;
+//     while (x < y) {
+//         x *= 1.10;
+//         d++;
+//     }
+// std::cout << d;
+//     }
+
+
+// // zadacha 4.1
+// #include <iostream>
+// #include <cmath>
+// #include <iomanip>
+// using namespace std;
+// int main() {
+//     double a,b;
+//     string znak;
+//     cout <<"vvedite virazhenie: \n";
+//     cin >> a >> znak >> b;
+//     if (znak == "+") {
+//         cout << "otvet: \n" << a + b;
+//     } else if (znak == "-") {
+//         cout << "otvet: \n" <<a - b;
+//     } else if (znak == "*") {
+//         cout << "otvet: \n" << a * b;
+//     } else if (znak == "/") {
+//         cout << "otvet: \n" << a / b;
+//     }
+// }
+
+// // zadacha 4.2
+// #include <iostream>
+// #include <cmath>
+// #include <iomanip>
+// using namespace std;
+// int main() {
+//     int a, b;
+//     cin >> a >> b;
+//     while (a!=b) {
+//         if (a%2==0) {
+//             a=a/2;
+//             cout << "/2";
+//         }
+//         else {
+//             a = a-1;
+//             cout << "-1";
+//         }
+//     }
+//     cout << a  << "==" << b;
+// }
+// // ya ne znayu
+
